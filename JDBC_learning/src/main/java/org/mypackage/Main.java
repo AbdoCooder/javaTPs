d8399681e478e557a4ad6b9635fb22b99e674050
Abdelkader Benajiba <abdocooder@gmail.com>
2025-02-26 17:21:35 +0000

    jdbc is working well now

--- JDBC_learning/src/main/java/org/mypackage/Main.java
+++ JDBC_learning/src/main/java/org/mypackage/Main.java
@@ -1,9 +1,51 @@
 package org.mypackage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		String jdbcUrl = "jdbc:mysql://localhost:3306/JDBC";
+		String username = "abenajib";
+		String password = "abenajib42";
+
+		try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password))
+		{
+			if (connection != null)
+			{
+				System.out.println("Connected to the database!");
+
+				String query = "SELECT * FROM users";
+				try (Statement statement = connection.createStatement();
+					 ResultSet resultSet = statement.executeQuery(query))
+					 {
+
+					while (resultSet.next())
+					{
+						int id = resultSet.getInt("id");
+						String user = resultSet.getString("username");
+						String email = resultSet.getString("email");
+						System.out.println("ID: " + id + ", Username: " + user + ", Email: " + email);
+					}
+				}
+			}
+			else
+				System.out.println("Failed to make connection!");
+		}
+		catch (SQLException e)
+		{
+			System.out.println("SQL Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+		finally
+		{
+			AbandonedConnectionCleanupThread.checkedShutdown();
+		}
 	}
 }
